24c25055198ac08812e19d628cfe0f0b939232ad
Bharath Raja <2320747+bigomega@users.noreply.github.com>
2023-07-18 22:34:39 +0530

    Writing complex testing scenarios

--- getting-started/README.md
+++ getting-started/README.md
@@ -1,5 +1,5 @@
 ## Driver version check
-Make sure that the version of `chromedriver` in [package.json](/getting-started/package.json) is close to your current Chrome browser version. You can check your Chrome version from here: `chrome://settings/help`.
+Make sure that the version of `chromedriver` in [package.json](/getting-started/package.json#L13) is close to your current Chrome browser version. You can check your Chrome version from here: `chrome://settings/help`.
 
 <img width="390" alt="image" src="https://github.com/nightwatchjs/examples-web-testing/assets/2320747/17d22e45-a6f5-4fcb-9512-e2820f28c00b">
 
@@ -18,4 +18,3 @@ npm test
 
 #### HTML Report
 <img width="1440" alt="image" src="https://github.com/nightwatchjs/examples-web-testing/assets/2320747/91787e00-898a-4a58-9b07-e5d7c06cf289">
-
--- getting-started/test/home.spec.js
+++ getting-started/test/home.spec.js
@@ -1,16 +1,72 @@
 describe('Nighwatch homepage', function () {
-  beforeEach(browser => browser.navigateTo('/'))
+  beforeEach(async function (browser) {
+    await browser.window.maximize()
+    await browser.navigateTo('/')
+  })
   afterEach(browser => browser.end())
 
   it('Should have the correct title', function(browser) {
     browser.assert.textEquals('h1', 'Introducing Nightwatch v3')
   })
 
-  it('Should lead to the installation page on click of Get Started', function(browser) {
+  it('Should lead to the installation page on click of Get Started', async function(browser) {
+    await browser.click(browser.element.findByText('Get Started'))
+    browser.assert.titleEquals('Getting Started | Developer Guide | Nightwatch.js')
+    browser.assert.equal(await browser.element.find('h1').getText(), 'Install Nightwatch')
+    const $filter_el = await browser.element.findByPlaceholderText('Filter by title')
+    browser.expect(await $filter_el.getAttribute('autocomplete')).to.equal('off')
+    browser.assert.urlContains('nightwatchjs.org/guide/quickstarts')
+  })
+
+  it('Should allow search and show correct results', async function(browser) {
+    await browser.click('#docsearch').waitForElementVisible('.DocSearch-Modal')
+    const $search_input = browser.element.findByPlaceholderText('Search docs')
+    await browser
+      .sendKeys($search_input, 'click ')
+      .pause(50) // Pausing for the JS tick
+      .waitForElementPresent('.DocSearch-Dropdown-Container')
+      .sendKeys($search_input, [browser.Keys.ARROW_DOWN, browser.Keys.ENTER])
+      .assert.textContains('h1', '.rightClick()')
+    ;
+  })
+
+  it('Should copy the installation command on copy button click', function (browser) {
+    const input_selector = '.DocSearch-Modal .DocSearch-Form input'
     browser
-      .click(browser.element.findByText('Get Started'))
-      .assert.titleEquals('Getting Started | Developer Guide | Nightwatch.js')
-      .assert.textEquals('h1', 'Install Nightwatch')
+      .click(browser.element.findByText('Copy'))
+      .click('#docsearch')
+      .sendKeys(input_selector, [browser.Keys.COMMAND, 'v', browser.Keys.NULL])
+      .assert.attributeMatches(input_selector, 'value', 'npm init nightwatch')
+    ;
+  })
+
+  it('Should should change with client script', async function (browser) {
+    const change_text = "{Client Side Execution}"
+    await browser.executeScript(function (new_text) {
+      const $hero_cta = document.querySelector('.hero__action-button--get-started')
+      $hero_cta.innerHTML = new_text
+      $hero_cta.style.background = '#ff7f2b'
+      document.querySelector('header .navigation-list').style.display = 'none'
+      document.querySelector('header .navigation__logo').style.width = '900px'
+    }, [change_text])
+    await browser.pause(1000) // Pausing just to notice the changes
+    await browser.click(browser.element.findByText(change_text))
+    browser.assert.titleMatches('Getting Started')
+  })
+
+  it('Should allow for substack subscription', function (browser) {
+    const iframe_selector = '.footer__wrapper-inner-social-subscribe iframe'
+    browser
+      .execute(function (iframe_selector) {
+        document.querySelector(iframe_selector).scrollIntoView()
+      }, [iframe_selector])
+      .setAttribute(iframe_selector, 'id', 'test-nightwatch-123')
+      .frame('test-nightwatch-123')
+      .sendKeys('input[type=email]', 'test@nightwatchjs.org')
+      .click('button[type=submit]')
+      .ensure.alertIsPresent()
+      .alerts.accept()
+      .assert.elementPresent(browser.element.findByText('Sign out'))
     ;
   })
 
@@ -24,15 +80,6 @@ describe('Nighwatch homepage', function () {
     const allWindows = await browser.window.getAllHandles()
     await browser.window.switchTo(allWindows[1])
 
-    await browser.assert.urlContains('https://github.com/nightwatchjs/')
-  })
-
-  it('Should copy the installation command on copy button click', function (browser) {
-    browser
-      .click(browser.element.findByText('Copy'))
-      .click('#docsearch')
-      .sendKeys('.DocSearch-Modal .DocSearch-Form input', [browser.Keys.COMMAND, 'v', browser.Keys.NULL])
-      .assert.attributeMatches('.DocSearch-Modal .DocSearch-Form input', 'value', 'npm init nightwatch')
-    ;
+    await browser.assert.urlContains('github.com/nightwatchjs')
   })
 })
